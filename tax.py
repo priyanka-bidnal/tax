a03acec4fc7ca4e9f2edb984e50b63ba47e73240
priyanka-bindal <bidnalpriyanka@gmail.com>
2025-10-23 16:20:39 +0530

    save

--- tax.py
+++ tax.py
@@ -1,4 +1,7 @@
 salary = float(input("Enter original salary: "))
-bonus_added = salary * 1.10  # salary + 10%
+bonus_added = salary * 1.10  # after 10% bonus
+after_tax = bonus_added * 0.95  # deduct 5% tax
+
 print(f"Original Salary: {int(salary) if salary.is_integer() else salary}")
 print(f"Bonus Added: {int(bonus_added) if bonus_added.is_integer() else bonus_added}")
+print(f"After Tax Deduction: {int(after_tax) if after_tax.is_integer() else after_tax}")
